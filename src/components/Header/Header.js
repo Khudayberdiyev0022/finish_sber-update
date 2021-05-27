24c3c690e91e815564319b67753c247fc62866f9
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-05-27 20:54:28 +0500

    12

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./Header.module.css";
 import iconSalute from "../../assets/svg/iconSalute.svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
