62cb5930b1b98c99489ed6e9ba96d0b447852086
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-05-28 14:26:58 +0500

    11

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,7 +1,8 @@
 import React from "react";
 import style from "./Section.module.css";
-import iconView from "../../assets/svg/iconView.svg";
 import iconNext from "../../assets/svg/iconNext.svg";
+import Frame1 from "../../assets/img/Frame1.png";
+import Frame2 from "../../assets/img/Frame2.png";
 
 function Section() {
   return (
@@ -10,30 +11,22 @@ function Section() {
         <div className={style.top}>
           <div className={style.left}>
             <div className={style.small}>
-              <h2>20 июня 2020 </h2>
               <h1>Как безопасно совершать покупки в интернете</h1>
               <div className={style.see}>
-                <div className={style.eye}>
-                  <img src={iconView} alt="iconView" />
-                  <h4>35</h4>
-                </div>
+                <h2>20 июня 2020 </h2>
                 <div className={style.next}>
                   <img src={iconNext} alt="iconNext" />
                 </div>
               </div>
             </div>
             <div className={style.middle}>
-              <h2>20 июня 2020 </h2>
               <h1>
                 Какие правила поведения в сети стоит ввести у себя в семье, как
                 наладить доверие, избежать детских истерик и родительского
                 гиперконтроля
               </h1>
               <div className={style.see}>
-                <div className={style.eye}>
-                  <img src={iconView} alt="iconView" />
-                  <h4>35</h4>
-                </div>
+                <h2>20 июня 2020 </h2>
                 <div className={style.next}>
                   <img src={iconNext} alt="" />
                 </div>
@@ -41,17 +34,16 @@ function Section() {
             </div>
           </div>
           <div className={style.right} id="bgGradient">
-            <div className={style.image}></div>
-            <h2>20 июня 2020 </h2>
+            <div
+              className={style.image}
+              style={{ backgroundImage: `url(${Frame1})` }}
+            ></div>
             <h1>
               Какие данные лучше не раскрывать в интернете - статья о
               приватности
             </h1>
             <div className={style.see}>
-              <div className={style.eye}>
-                <img src={iconView} alt="iconView" />
-                <h4>35</h4>
-              </div>
+              <h2>20 июня 2020 </h2>
               <div className={style.next}>
                 <img src={iconNext} alt="" />
               </div>
@@ -59,18 +51,17 @@ function Section() {
           </div>
         </div>
         <div className={style.center}>
-          <div className={style.image}></div>
+          <div
+            className={style.image}
+            style={{ backgroundImage: `url(${Frame2})` }}
+          ></div>
           <div className={style.content}>
-            <h2>20 июня 2020 </h2>
             <h1>
               Приложения и программы для родителей и детей, которые помогут
               защитить ребенка в сети
             </h1>
             <div className={style.see}>
-              <div className={style.eye}>
-                <img src={iconView} alt="iconView" />
-                <h4>35</h4>
-              </div>
+              <h2>20 июня 2020 </h2>
               <div className={style.next}>
                 <img src={iconNext} alt="" />
               </div>
@@ -79,29 +70,21 @@ function Section() {
         </div>
         <div className={style.bottom}>
           <div className={style.small}>
-            <h2>20 июня 2020 </h2>
             <h1>
               Как избежать кибербуллинга и как помочь своему ребенку, если это
               все-таки случилось
             </h1>
             <div className={style.see}>
-              <div className={style.eye}>
-                <img src={iconView} alt="iconView" />
-                <h4>35</h4>
-              </div>
+              <h2>20 июня 2020 </h2>
               <div className={style.next}>
                 <img src={iconNext} alt="" />
               </div>
             </div>
           </div>
           <div className={style.small}>
-            <h2>20 июня 2020 </h2>
             <h1>Как уберечь ребенка от вредного и опасного контента в сети</h1>
             <div className={style.see}>
-              <div className={style.eye}>
-                <img src={iconView} alt="iconView" />
-                <h4>35</h4>
-              </div>
+              <h2>20 июня 2020 </h2>
               <div className={style.next}>
                 <img src={iconNext} alt="" />
               </div>
